9e772959172e1c6e44981909dddb5a3327dbae56
HenruMatthis <u23526158@tuks.co.za>
2025-05-22 18:48:40 +0200

    made Loginpage.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import "./App.css";
+import atatImage from "./img/atat.png"; // Put your AT-AT image in src/atat.png
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <div className="left-panel">
+        <h1>Welcome to AT-AT</h1>
+        <button className="login-btn">LOGIN</button>
+
+        <button className="google-btn">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+            alt="Google Logo"
+          />
+          Continue with Google
+        </button>
+
+        <div className="or-separator">OR</div>
+
+        <input
+          type="email"
+          placeholder="email@domainName.com"
+          className="input-field"
+        />
+        <input type="password" placeholder="password" className="input-field" />
+
+        <button className="continue-btn">Continue</button>
+
+        <p className="signup-text">
+          Need an account? <a href="#signup">SignUp</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="right-panel">
+        <img src={atatImage} alt="AT-AT" />
+      </div>
     </div>
   );
 }

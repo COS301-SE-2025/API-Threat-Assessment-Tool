968f551cef66254bc7e5d2371f3a0667122dccbe
DragonMage899 <justinbhana12@gmail.com>
2025-06-18 19:22:16 +0200

    woof

---
+++ backend/utils/user_manager.py
@@ -0,0 +1,24 @@
+# user management 
+
+class UserManager:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.connection = None
+
+    def openDB(self):
+        print("do something")
+
+    def newUser(self):
+        print("do something")
+
+    def updateUser(self, report):
+        print("do something")
+
+    def removeUser(self, report):
+        print("do something")
+
+    def setUserPermissions(self):
+        print("do something")
+
+    def close(self):
+        print("do something")

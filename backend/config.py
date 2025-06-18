a1299cb55dff425da142337c8e63f153fa87ebae
DragonMage899 <justinbhana12@gmail.com>
2025-06-18 19:09:13 +0200

    woof

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+# config.py
+DEFAULT_TIMEOUT = 10
+MAX_THREADS = 5
+DEFAULT_HEADERS = {
+    "User-Agent": "APIScanner/1.0"
+}
+LOG_LEVEL = "INFO"
+DATABASE_PATH = "
+SUPPORTED_SPEC_FORMATS = ["openapi", "swagger", "postman"]
---
+++ backend/core/result_manager.py
@@ -0,0 +1,17 @@
+# Stores results created by the VulnerabilityScanner
+
+class ResultManager:
+    def __init__(self):
+        self.apiName = ""
+        self.scans = {"some key value" : [ScanResult]}
+
+    def add_result():
+        print("do something")
+
+    def get_result():
+        print("do something")
+
+    def remove_result():
+        print("do something")
+
+

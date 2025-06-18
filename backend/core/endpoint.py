0479e31eff8701b3b67ca1a767973c0ad2acd4c3
DragonMage899 <justinbhana12@gmail.com>
2025-06-18 19:02:53 +0200

    defined Endpoint class skeleton

---
+++ backend/core/endpoint.py
@@ -0,0 +1,17 @@
+# Individual Endpoint definitions of the api we're scanning  
+# Probably need more variables, research api specification docs to get all common ones
+
+class Endpoint:
+    def __init__(self):
+        self.name = ""
+        self.requestType = ""
+        self.tags = ""
+        self.category = ""
+    
+    #getters and setters for each variable
+    def set_value():
+        print("do something")
+
+    def get_value():
+        print("do something")
+

8cef2b0f71731af174c86d881b9f539e35163e73
DragonMage899 <justinbhana12@gmail.com>
2025-06-18 19:08:32 +0200

    defined db skeleton

---
+++ backend/utils/db.py
@@ -0,0 +1,18 @@
+# db management
+
+class DatabaseManager:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.connection = None
+
+    def connect(self):
+        print("do something")
+
+    def insert_data(self, report):
+        print("do something")
+
+    def fetch_data(self):
+        print("do something")
+
+    def close(self):
+        print("do something")

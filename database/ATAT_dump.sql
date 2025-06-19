6a261a3a23f5ed20d9cc5fb2c73e0417d5118887
HenruMatthis <u23526158@tuks.co.za>
2025-06-19 17:02:48 +0200

    Made APIs Table

--- database/ATAT_dump.sql
+++ database/ATAT_dump.sql
@@ -6,3 +6,11 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     role TEXT DEFAULT 'user'  -- can be 'user', 'admin', etc.
 );
+CREATE TABLE apis (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,  -- owner
+    name TEXT NOT NULL,
+    base_url TEXT NOT NULL,
+    imported_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);

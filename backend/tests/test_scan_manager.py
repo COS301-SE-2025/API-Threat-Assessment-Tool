d5d843e348607eb0708df97244d50f609d9ecbd6
DragonMage899 <justinbhana12@gmail.com>
2025-08-17 16:06:48 +0200

    Implemented Stuff

--- backend/tests/test_scan_manager.py
+++ backend/tests/test_scan_manager.py
@@ -1,4 +1,5 @@
-#venv/bin/python -m tests.temp-tests  
+#cd backend
+#venv/bin/python -m tests.test_scan_manager
 import os
 
 print("start test")
@@ -7,7 +8,7 @@ print("Custom Server Load")
 
 from input.api_importer import APIImporter
 from core.api_client import APIClient
-from core.vulnerability_test import VulnerabilityTest, BOLA_Scan, OWASP_FLAGS
+from core.vulnerability_test import VulnerabilityTest, BOLA_Scan, BKEN_AUTH_Scan, BOPLA_Scan, URC_Scan, BFLA_Scan, UABF_Scan, SSRF_Scan, SEC_MISC_Scan, IIM_Scan, UCAPI_Scan, OWASP_FLAGS
 from core.scan_manager import ScanManager, ScanProfiles
 
 JWT = "weak_secret_123"
@@ -42,10 +43,14 @@ for endpoint in api_client.endpoints:
         print("Path: ", endpoint.path)
 
 
+
+print("=====================================================================")
+print("==========================\nBOLA Testing\n==========================")
+print("=====================================================================")
+
 bola_scanner = BOLA_Scan()
 print(bola_scanner.name.value)
-
-print("set flags")
+print("setting flags")
 bola_scanner._set_flag(api_client)
 
 print("\nEndpoints with the BOLA flag:")
@@ -54,28 +59,89 @@ for endpoint in api_client.endpoints:
         if flag == OWASP_FLAGS.BOLA:
             print(endpoint.path)
 
-# print("Test identified endpoints for BOLA")
-# result = bola_scanner.run_test(api_client)
-# print(result.to_json())
+print("=====================================================================")
+print("==========================\nBOPLA Testing\n==========================")
+print("=====================================================================")
+
+bola_scanner = BOPLA_Scan()
+print(bola_scanner.name.value)
+print("setting flags")
+bola_scanner._set_flag(api_client)
+
+print("\nEndpoints with the BOPLA flag:")
+for endpoint in api_client.endpoints:
+    for flag in endpoint.flags:
+        if flag == OWASP_FLAGS.BOPLA:
+            print(endpoint.path)
+
+print("=====================================================================")
+print("==========================\nBFLA Testing\n==========================")
+print("=====================================================================")
+
+bfla_scanner = BFLA_Scan()
+print(bfla_scanner.name.value)
+print("setting flags")
+bfla_scanner._set_flag(api_client)
+
+print("\nEndpoints with the BFLA flag:")
+for endpoint in api_client.endpoints:
+    for flag in endpoint.flags:
+        if flag == OWASP_FLAGS.BFLA:
+            print(endpoint.path)
+
+print("=====================================================================")
+print("==========================\nSSRF Testing\n==========================")
+print("=====================================================================")
+
+ssrf_scanner = SSRF_Scan()
+print(ssrf_scanner.name.value)
+print("setting flags")
+ssrf_scanner._set_flag(api_client)
+
+print("\nEndpoints with the SSRF flag:")
+for endpoint in api_client.endpoints:
+    for flag in endpoint.flags:
+        if flag == OWASP_FLAGS.SSRF:
+            print(endpoint.path)
+
+print("=====================================================================")
+print("==========================\nIIM Testing\n==========================")
+print("=====================================================================")
+
+iim_scanner = IIM_Scan()
+print(iim_scanner.name.value)
+print("setting flags")
+iim_scanner._set_flag(api_client)
+
+print("\nEndpoints with the IIM flag:")
+for endpoint in api_client.endpoints:
+    for flag in endpoint.flags:
+        if flag == OWASP_FLAGS.IIM:
+            print(endpoint.path)
 
 # ==============================================================================
-print("Scan Management Testing")
-scan_manager = ScanManager(api_client)
-scan_manager.createScan(ScanProfiles.DEFAULT)
-scan_id = scan_manager.runScan(ScanProfiles.DEFAULT)
+print("=====================================================================")
+print("==========================\nScan Testing\n==========================")
+print("=====================================================================")
 
-#run 10x
-# for i in range(10):
-#     scan_manager.runScan(ScanProfiles.DEFAULT)
+run_scans = True
 
-print(f"Scan complete with id: {scan_id}")
-print("All scans:")
-for scanID in scan_manager.get_all_scans():
-    print(f"Scan id: {scanID}")
-    scans = scan_manager.get_scan(scanID)
-    for scan in scans:
-        print(f"\tEndpoint Details:")
-        print(f"\t\tEndpoint: {scan.endpoint.path}")
-        print(f"\t\tCategory: {scan.owasp_category}")
-        print(f"\t\tDescription: {scan.description}")
+if(run_scans):
+    scan_manager = ScanManager(api_client)
+    scan_manager.createScan(ScanProfiles.DEFAULT)
+    scan_id = scan_manager.runScan(ScanProfiles.DEFAULT)
 
+    print(f"Scan complete with id: {scan_id}")
+    print("All scans:")
+    for scanID in scan_manager.get_all_scans():
+        print(f"Scan id: {scanID}")
+        scans = scan_manager.get_scan(scanID)
+        for scan in scans:
+            for scan_endpoint in scan:
+                print(f"\tEndpoint Details:")
+                print(f"\t\tEndpoint: {scan_endpoint.endpoint.path}")
+                print(f"\t\tOWASP Category: {scan_endpoint.owasp_category}")
+                print(f"\t\tDescription: {scan_endpoint.description}")
+                # print(f"Evidence: \n------------------------------\n{scan_endpoint.evidence}\n------------------------------\n")
+
+# ==============================================================================

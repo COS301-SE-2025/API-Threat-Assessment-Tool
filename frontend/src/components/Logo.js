1fd9566425c2932b43a3bfc6c86dd64f172c70c2
HenruMatthis <u23526158@tuks.co.za>
2025-09-26 00:23:42 +0200

    Added new logo and also Seperated hrefs

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -2,34 +2,48 @@ import React, { useContext } from "react";
 import { ThemeContext } from "../App";
 import logoBlue from "../img/logo-blue.png";
 import logoWhite from "../img/logo-white.png";
-import ATAT from "../img/ATAT_logo.png";
+import ATAT from "../img/YESSSS.png";
 
 const Logo = ({ className = "", style = {}, ...props }) => {
   const { darkMode } = useContext(ThemeContext);
   const logoSrc = darkMode ? logoWhite : logoBlue;
 
   return (
-    <a
-      href="https://bitm.co.za"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ display: "flex", alignItems: "center", textDecoration: "none" }}
-      tabIndex={0}
-      aria-label="Visit BITM website"
-    >
-      <img
-        src={logoSrc}
-        alt="BlueVision Logo"
-        className={`logo-img ${className}`}
-        style={{ height: 70, ...style }}
-        {...props}
-      />
-      <img
-        src={ATAT}
-        alt="AT-AT Logo"
-        style={{ height: 70, marginLeft: 8 }}
-      />
-    </a>
+    <div style={{ display: "flex", alignItems: "center" }}>
+      {/* BlueVision logo */}
+      <a
+        href="https://bitm.co.za"
+        target="_blank"
+        rel="noopener noreferrer"
+        tabIndex={0}
+        aria-label="Visit BITM website"
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <img
+          src={logoSrc}
+          alt="BlueVision Logo"
+          className={`logo-img ${className}`}
+          style={{ height: 70, ...style }}
+          {...props}
+        />
+      </a>
+
+      {}
+      <a
+        href="http://apithreatassessment.co.za/"
+        target="_blank"
+        rel="noopener noreferrer"
+        tabIndex={0}
+        aria-label="Visit AT-AT project repository"
+        style={{ display: "flex", alignItems: "center", marginLeft: 8 }}
+      >
+        <img
+          src={ATAT}
+          alt="AT-AT Logo"
+          style={{ height: 70 }}
+        />
+      </a>
+    </div>
   );
 };
 
